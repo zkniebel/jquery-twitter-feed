db2f2995d4a910618411ee4e7c43039027a3ddc3
Zachary Kniebel <contact@zacharykniebel.com>
2013-03-27 10:25:32 -0400

    Added example call to plugin

---
+++ example.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+        $("#feedTarget").twitterFeed({
+		        count: 4,
+                rawData: yourRawJSONData,
+                screen_name: twitterHandleToQuery,
+                prepend: "<div class='tweetWrapper'>", 
+                append: "</div>",
+                tweetBodyClass: "tweetBody tweetText",
+		        date: { prepend: "<div>", append: " - ", order: 3, cssClass: "tweetDate" },
+		        retweet: { show: false },
+		        favorite: { prepend: " - ", order: 0, append: "</div>" },
+		        callbackOnEach: true,
+		        callback: function() { 
+                $(this).find(".tweetBody").myCallbackOnEachTweet(); 
+            }
+	      });
+    });
+});
